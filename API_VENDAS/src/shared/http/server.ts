3a909be4b5e9de2feb552593e4866f5ecc9ad942
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-03-01 11:41:43 -0300

    [feat] add class appError

--- API_VENDAS/src/shared/http/server.ts
+++ API_VENDAS/src/shared/http/server.ts
@@ -1,6 +1,8 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
@@ -8,6 +10,19 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+// middleware
+app.use((error: Error, req: Request, res: Response) => {
+  if (error instanceof AppError) {
+    return res
+      .status(error.statusCode)
+      .json({ status: 'error', message: error.message });
+  }
+
+  return res
+    .status(500)
+    .json({ status: 'error', message: 'Internal server error' });
+});
 
 app.listen(5000, () => {
   console.log('Server rodando em: http://localhost:5000');
